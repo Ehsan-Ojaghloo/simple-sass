96c2d8b83bd8f48d4fc1bba2f0e39678aef81bad
Ehsan Ojaghloo <e.ojaghloo86@gmail.com>
2024-05-16 14:37:07 +0330

    added a new theme for site

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import "./pages/_App.scss"
 
 function App() {
   return (
-    <div className="container">
+    <div className="container yellow-theme">
       <div className="nav-container">
         <nav>
           <ul>
-            <li> Home </li>
-            <li> About us </li>
-            <li> Contact us </li>
+            <li className='yellow-theme'> Home </li>
+            <li className='yellow-theme'> About us </li>
+            <li className='yellow-theme'> Contact us </li>
           </ul>
           <img src="/react-logo.png" alt="logo" />
-          <button> Sign up </button>
+          <button className='yellow-theme'> Sign up </button>
         </nav>
       </div>
       <div className="hero-container">
         <div className="hero-inner">
-          <h1> There is a lot of <span>features</span> in our website and lots of <span>choices</span> to make your dream website. </h1>
-          <img src="/hero-img.png" alt="image" />
+          <div>
+            <h1> There is a lot of <span className='yellow-theme'>features</span> in our website and lots of <span className='yellow-theme'>choices</span> to make your <span className='yellow-theme'>dream</span> website. </h1>
+            <button className='more-btn yellow-theme'> More... </button>
+          </div>
+          <img src="/hero-img.svg" alt="image" />
         </div>
       </div>
     </div>
